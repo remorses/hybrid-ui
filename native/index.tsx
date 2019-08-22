c786bfebf80c472e517b6f1662e0366e0692c8e8
remorses <beats.by.morse@gmail.com>
2019-08-22 18:33:19 +0200

    fix

--- native/index.tsx
+++ native/index.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
-import { box, text, variants } from '../styleProps'
+import { box, text, variants } from '../src/styleProps'
 import {
     BoxProps,
     TextProps,
     ButtonProps,
     ImageProps,
     ScrollViewProps
-} from '../types'
+} from '../src/types'
