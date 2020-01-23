b0d87279099afc6334190ec7e3ef7c57756ab891
remorses <beats.by.morse@gmail.com>
2020-01-23 17:35:57 +0100

    added example

---
+++ example/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Box } from '../src'
+import { ThemeProvider } from 'styled-components'
+import { render } from 'react-dom'
+
+const theme = {
+    colors: {
+        primary: 'blue'
+    }
+}
+
+const App = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Box bg='primary' width='20px' height='40px'></Box>
+        </ThemeProvider>
+    )
+}
+
+render(<App />, document.getElementById('root'))
+
+// @ts-ignore
+if (module.hot) {
+    // @ts-ignore
+    module.hot.accept()
+}
